a4301b3fd16f5f1e5034376ded750c7d7948ae23
root <root@ip-172-31-3-91.us-west-2.compute.internal>
2017-11-01 08:52:21 +0000

    first commit

---
+++ listCollection.js
@@ -0,0 +1,20 @@
+var AWS = require('aws-sdk');
+
+// Load credentials and set region from JSON file
+AWS.config.loadFromPath('./config.json');
+
+var rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+
+var params = {
+ };
+
+ rekognition.listCollections(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+   /*
+   data = {
+    CollectionArn: "aws:rekognition:us-west-2:123456789012:collection/myphotos", 
+    StatusCode: 200
+   }
+   */
+ });
